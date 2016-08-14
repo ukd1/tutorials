5ebf40b3d45b2b868d8c258086c14273ca1826fc
Octopus in vitro <octopusinvitro@users.noreply.github.com>
2016-08-14 16:40:10 +0100

    Update User Interface to read filepaths from the markup
    The tests were updated with fixtures to simulate the current state of the markup, since now the file paths are served in a list of links in the tutorial page.

--- javascripts-dev/src/ui.js
+++ javascripts-dev/src/ui.js
@@ -1,19 +1,20 @@
 var UserInterface = {
-  selector: "a[href=download]"
+  selector: "a[href^='#download-']"
 };
 
 UserInterface.setup = function(zipper, downloader) {
 
   var downloadLink = document.body.querySelector(UserInterface.selector);
 
-  var createZip = function() {
-    zipper.createZip(downloader);
+  var createZip = function(downloadLinkUrl) {
+    var listId = downloadLinkUrl.split("#").pop();
+    zipper.createZip(downloader, UserInterface.getPathsInList(listId));
   };
 
   var registerListener = function(link) {
     link.addEventListener("click", function(event) {
       event.preventDefault();
-      createZip();
+      createZip(this.href);
     }, false);
   };
 
@@ -24,4 +25,15 @@ UserInterface.setup = function(zipper, downloader) {
   };
 
   checkIfDownloadLinkExist();
-}
+};
+
+UserInterface.getPathsInList = function(listId) {
+  var i, n, paths = [],
+      links = document.querySelectorAll("#" + listId + " a");
+
+  for (i=0, n=links.length; i<n; i++) {
+    paths.push(links[i].href);
+  }
+
+  return paths;
+};
